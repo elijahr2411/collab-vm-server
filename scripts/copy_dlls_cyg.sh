e3d828bb019b035b737ab4cb5266f3677a9ca0f8
6-9-6-9 <annastockmeier@gmail.com>
2021-01-18 03:25:35 -0500

    Whoops, fixed

--- scripts/copy_dlls_cyg.sh
+++ scripts/copy_dlls_cyg.sh
@@ -3,7 +3,7 @@
 cp cvmlib/bin/*.dll $2
 LIST=$(ldd $2/collab-vm-server.exe | cut -d '>' -f2 | cut -d '(' -f1 | cut -d ' ' -f2 | sed -E '/\/c\//d' | sed -E '/\/home\//d' | sed -E '/not/d')
 
-if [ ! $LIST ]; then exit 0; fi
+if [ -z "$LIST" ]; then exit 0; fi
 
 echo $LIST
 cp $LIST $2 2>/dev/null
